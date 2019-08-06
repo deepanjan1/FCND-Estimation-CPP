bf52e4f68e3d438734ec9f0f43acd6ffb07bf172
Deepanjan De <deepanjande@Deepanjans-MacBook.local>
2019-08-06 13:40:12 -0400

    working on mag update

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -337,7 +337,16 @@ void QuadEstimatorEKF::UpdateFromMag(float magYaw)
   //  - The magnetomer measurement covariance is available in member variable R_Mag
   ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
 
+  hPrime(0,6) = 1;
+  zFromX(0) = ekfState(6);
 
+  float delta = magYaw - ekfState(6);
+
+  if (delta > F_PI) {
+    zFromX(0) += 2.f * F_PI;
+  } else if (delta < F_PI) {
+    zFromX(0) -= 2.f * F_PI;
+  }
   /////////////////////////////// END STUDENT CODE ////////////////////////////
 
   Update(z, hPrime, R_Mag, zFromX);
